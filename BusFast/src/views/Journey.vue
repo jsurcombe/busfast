e8163ab92e0c93d91b973a49c9d7ec2cc59b406b
John Surcombe <john@surcombe.com>
2021-09-20 15:40:21 +0100

    tweak

--- BusFast/src/views/Journey.vue
+++ BusFast/src/views/Journey.vue
@@ -9,41 +9,10 @@
 
 <script lang="ts">
     import { Vue } from 'vue-class-component';
-    import Api, { ClusterItem } from '@/api';
     
     export default class Journey extends Vue {
 
-        fromStopQ: string = '';
-        fromStop: ClusterItem | null = null;
-
-        fromStops: ClusterItem[] | null = null;
-
-        changedFromQ(q: string) {
-            this.fromStopQ = q;
-            this.getStops();
-        }
-
         mounted() {
-        }
-
-        getStops() {
-            if (this.fromStopQ) {
-                const q = this.fromStopQ;
-                Api.getStops(this.fromStopQ)
-                    .then(response => {
-                        if (q === this.fromStopQ)
-                            this.fromStops = response.data;
-                    });
-
-            } else {
-                this.fromStops = [];
-            }
-        }
-
-        selectFromStop(stop: ClusterItem) {
-            this.fromStop = stop;
-            this.fromStopQ = stop.name;
-            this.fromStops = null;
         }
     }
 </script>
